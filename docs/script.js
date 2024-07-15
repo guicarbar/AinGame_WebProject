ee828051a8d73d7ae40518bf65abaf518e98b990
guicarbar <guicarbar@gmail.com>
2024-07-15 02:33:13 -0300

    update

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,12 @@
 const starText = `
-    <div class="ballpoint" id="ball1" onclick="position(gameBall1)"></div>
-    <div class="ballpoint" id="ball2" onclick="position(gameBall2)"></div>
-    <div class="ballpoint" id="ball3" onclick="position(gameBall3)"></div>
+    <div class="ballpoint" id="ball1"></div>
+    <div class="ballpoint" id="ball2"></div>
+    <div class="ballpoint" id="ball3"></div>
+    <div class="ballerro" id="ballerro"></div>
+    <div class="balldp" id="balldp"></div>
 `
 
-const redBall = `<div class="ballerro" id="ballerro" onclick="position(gameBallerro)"></div>`
-const especialBall = `<div class="balldp" id="balldp" onclick="position(gameballdp)"></div>`
+const timerBar = document.getElementById('timerBar')
 
 const gameArea = document.getElementById('game')
 
@@ -21,33 +22,40 @@ function position(ballClick) {
     ballClick.style.left = randoMedidas(largura) + 'px'
 }
 
+function addpoint() {
+    const pontos = document.getElementById('pontos')
+    const valuePontos = parseInt(pontos.innerHTML)
+
+    pontos.innerHTML = valuePontos + 100
+}
+
+function startBar() {
+    timerBar.classList.add('timer-before')
+}
+
+function timeEnd() {
+    setTimeout(() => {gameArea.innerHTML = ''}, 60000)
+}
+
 function start() {
     gameArea.innerHTML = starText
-    
+
     const gameBall1 = document.getElementById('ball1')
     const gameBall2 = document.getElementById('ball2')
     const gameBall3 = document.getElementById('ball3')
     const gameBallErro = document.getElementById('ballerro')
-    const gameballDp = document.getElementById('balldp')
-    
-    gameBall1.addEventListener('click', () => position(gameBall1))
-    gameBall2.addEventListener('click', () => position(gameBall2))
-    gameBall3.addEventListener('click', () => position(gameBall3))
-    gameBallErro.addEventListener('click', () => position(gameBallErro))
-    gameballDp.addEventListener('click', () => position(gameballDp))
+    const gameBallDp = document.getElementById('balldp')
 
     position(gameBall1)
     position(gameBall2)
     position(gameBall3)
-    timerRedBall()
+    position(gameBallErro)
+    position(gameBallDp)
+
+    gameBall1.addEventListener('click', () => {position(gameBall1), addpoint()})
+    gameBall2.addEventListener('click', () => {position(gameBall2), addpoint()})
+    gameBall3.addEventListener('click', () => {position(gameBall3), addpoint()})
+    gameBallErro.addEventListener('click', () => {position(gameBallErro), addpoint()})
+    gameBallDp.addEventListener('click', () => {position(gameBallDp), addpoint()})
 }
 
-function redBallInput() {
-    var html = gameArea.innerHTML
-
-    gameArea.innerHTML = html + redBall
-}
-
-function timerRedBall() {
-    setTimeout(redBallInput(), 8000)
-}
