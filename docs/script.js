a0372116b7ed3e3bb61bfb27d9007b333e3983a6
guicarbar <guicarbar@gmail.com>
2024-07-15 23:48:29 -0300

    update

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
+var life = 5
+var lifeBar = document.getElementById('life')
+
 const starText = `
     <div class="ballpoint" id="ball1"></div>
     <div class="ballpoint" id="ball2"></div>
@@ -7,9 +10,7 @@ const starText = `
 `
 
 const timerBar = document.getElementById('timerBar')
-
 const gameArea = document.getElementById('game')
-
 const largura = gameArea.offsetWidth
 const altura = gameArea.offsetHeight
 
@@ -25,8 +26,13 @@ function position(ballClick) {
 function addpoint() {
     const pontos = document.getElementById('pontos')
     const valuePontos = parseInt(pontos.innerHTML)
-
     pontos.innerHTML = valuePontos + 100
+}
+
+function addDoublepoint() {
+    const pontos = document.getElementById('pontos');
+    const valuePontos = parseInt(pontos.innerHTML);
+    pontos.innerHTML = valuePontos + 500
 }
 
 function startBar() {
@@ -34,28 +40,103 @@ function startBar() {
 }
 
 function timeEnd() {
-    setTimeout(() => {gameArea.innerHTML = ''}, 60000)
+    setTimeout(() => {
+        gameArea.innerHTML = ''
+    }, 60000)
+}
+
+function verificarLife() {
+    if (life === 0) {
+        gameArea.innerHTML = ''
+    } else {
+        console.log('continuar game')
+    }
+}
+
+function errou() {
+    life -= 1
+    let lifeVar = lifeBar.offsetWidth
+    lifeVar -= 40
+    lifeBar.style.width = lifeVar + 'px'
+}
+
+function dbBall() {
+    const gameBallDp = document.getElementById('balldp');
+    position(gameBallDp)
+    gameBallDp.style.display = 'none'
+
+    setTimeout(() => {
+        gameBallDp.style.display = 'block'
+        gameBallDp.addEventListener('click', () => {
+            addDoublepoint()
+            dpBallReloading(gameBallDp);
+        });
+    }, 5000)
+}
+
+function timeToClick(ball) {
+    function resetTimer() {
+        clearTimeout(ball.timer)
+        ball.timer = setTimeout(() => {
+            position(ball)
+            resetTimer()
+        }, 4500)
+    }
+    resetTimer()
+}
+
+function dpBallReloading(gameBallDp) {
+    position(gameBallDp)
+    if (gameBallDp.style.display === 'block') {
+        gameBallDp.style.display = 'none'
+    } else {
+        console.log('bolinha bala')
+    }
+    setTimeout(() => {
+        gameBallDp.style.display = 'block'
+    }, 7000)
 }
 
 function start() {
-    gameArea.innerHTML = starText
+    gameArea.innerHTML = starText;
 
     const gameBall1 = document.getElementById('ball1')
     const gameBall2 = document.getElementById('ball2')
     const gameBall3 = document.getElementById('ball3')
     const gameBallErro = document.getElementById('ballerro')
-    const gameBallDp = document.getElementById('balldp')
 
     position(gameBall1)
     position(gameBall2)
     position(gameBall3)
     position(gameBallErro)
-    position(gameBallDp)
 
-    gameBall1.addEventListener('click', () => {position(gameBall1), addpoint()})
-    gameBall2.addEventListener('click', () => {position(gameBall2), addpoint()})
-    gameBall3.addEventListener('click', () => {position(gameBall3), addpoint()})
-    gameBallErro.addEventListener('click', () => {position(gameBallErro), addpoint()})
-    gameBallDp.addEventListener('click', () => {position(gameBallDp), addpoint()})
+    function resetTimer(ball) {
+        clearTimeout(ball.timer)
+        ball.timer = setTimeout(() => {
+            position(ball)
+            resetTimer(ball)
+        }, 4500)
+    }
+
+    gameBall1.addEventListener('click', () => {
+        position(gameBall1)
+        addpoint()
+        resetTimer(gameBall1)
+    });
+    gameBall2.addEventListener('click', () => {
+        position(gameBall2)
+        addpoint()
+        resetTimer(gameBall2)
+    });
+    gameBall3.addEventListener('click', () => {
+        position(gameBall3)
+        addpoint()
+        resetTimer(gameBall3)
+    });
+    gameBallErro.addEventListener('click', () => {
+        position(gameBallErro)
+        errou()
+        verificarLife()
+        resetTimer(gameBallErro)
+    });
 }
-
